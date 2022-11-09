4dbfb4c823588a5558071846d5a9d934efccfe0f
krasi1 <krissi.petrov@gmail.com>
2022-11-09 15:58:27 +0200

    adding methods and properties

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,6 @@
 import EventEmitter from "eventemitter3";
 
+let Data;
 export default class Application extends EventEmitter {
   static get events() {
     return {
@@ -9,12 +10,39 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
-
+    this._loading = document.getElementById("progbar");
     this.emit(Application.events.READY);
+    this._startLoading(this._loading);
+    this._load("https://swapi.boom.dev/api/planets");
+  }
+  async _load(url) {
+    const rawData = await fetch(url);
+    Data = await rawData.json();
+    this._create(Data);
+    if (Data.next === null) {
+
+      this._stopLoading(this._loading);
+      return;
+    }
+    this._load(Data.next);
+  }
+
+  _create(object) {
+    //console.log(object.results);
+    for(let i = 0; i < object.results.length; i++)
+    {
+      let x = document.createElement('div');
+      x.innerHTML = object.results[i].name;
+      document.body.appendChild(x);
+      //console.log(x);
+    }
+  }
+
+  _startLoading(){
+    //this._loading.style.visibility = "hidden";
+  }
+
+  _stopLoading(){
+    this._loading.style.visibility = "hidden";
   }
 }
